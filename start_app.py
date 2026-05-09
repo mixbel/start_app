8e4cce05b2c4e09ce8bc508bde34d208f188ceae
mixbel <mixbel2811@gmail.com>
2026-05-09 18:56:19 +0300

    Update start_app.py

--- start_app.py
+++ start_app.py
@@ -212,7 +212,7 @@ st.set_page_config(
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.image("89120564-6.png", width=300)
-    st.markdown("<h1 style='text-align: center;'>Калькулятор от Gazminer</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;margin-top: -15px;'>Калькулятор от Gazminer</h1>", unsafe_allow_html=True)
 st.markdown("---")
 # ===================================================
 # ========== КАСТОМНЫЙ ДИЗАЙН ==========
